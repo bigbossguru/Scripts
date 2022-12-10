f83a879c9473398aa7e859a75459b35a7ce78e88
Eldar Gazizov <kursus1010@gmail.com>
2022-12-10 22:25:58 +0100

    rework all reactjs app structure with typescript and redux

--- ReactJS_startProject.sh
+++ ReactJS_startProject.sh
@@ -8,23 +8,36 @@
 
 set -e
 
+echo "-----------------------------"
 echo "Start generate project folder"
 echo "-----------------------------"
-
-cd project || mkdir project
-cd project || :
 npx create-react-app frontend --template typescript
 
-printf "## Frontend\n
-[Frontend Manual](frontend/README.md)\n" >> README.md
 
+echo "-----------------------------------------------"
+echo "Install all necessary libraries and dependencie"
+echo "-----------------------------------------------"
 cd frontend
 npm install axios
 npm install react-router-dom
 npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
-npm install @reduxjs/toolkit react-redux
+npm install @reduxjs/toolkit react-redux @types/react-redux
 
+
+echo "-------------------------"
+echo "Preparing default folders"
+echo "-------------------------"
+mkdir src/components
+mkdir src/store
+mkdir src/models
+mkdir src/hooks
+mkdir src/pages
+
+
+echo "------------------------------"
+echo "Preparing default boilerplates"
+echo "------------------------------"
 printf "/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -40,23 +53,68 @@ printf "@tailwind base;
 @tailwind components;
 @tailwind utilities;" > src/index.css
 
-#touch Dockerfile entrypoint.sh .env .dockerignore
+printf "import React from \"react\";
+import ReactDOM from \"react-dom/client\";
+import \"./index.css\";
+import App from \"./App\";
+import { store } from \"./store\";
+import { BrowserRouter } from \"react-router-dom\";
+import { Provider } from \"react-redux\";
 
-#mkdir src/api
-mkdir src/components
-mkdir src/store
-mkdir src/models
-#mkdir src/context
-#mkdir src/hoc
-mkdir src/hooks
-#mkdir src/layout
-mkdir src/pages
+const root = ReactDOM.createRoot(
+  document.getElementById(\"root\") as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);" > src/index.tsx
 
+printf "import React from \"react\";
+import { Route, Routes } from \"react-router-dom\";
+import HomePage from \"./pages/HomePage\";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path=\"/\" element={<HomePage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;" > src/App.tsx
+
+printf "import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>HomePage</div>
+  )
+}" > src/pages/HomePage.tsx
+
+printf "import { configureStore } from \"@reduxjs/toolkit\";
+
+
+export const store = configureStore({
+  reducer: {
+
+  }
+});" > src/store/index.ts
+
+
+echo "------------------------------------"
+echo "Removing unnecessary files and dirs"
+echo "------------------------------------"
 rm src/App.css
 rm src/App.test.tsx
 rm src/logo.svg
 rm src/reportWebVitals.ts
 rm -rf .git
 
-echo "-----------------------------"
+echo "------------------------------------"
 echo Finished $@
+echo "------------------------------------"
